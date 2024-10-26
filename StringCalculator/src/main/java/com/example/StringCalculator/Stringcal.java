6fe9da3b2def8483a154470493d35e53b1f452da
glakade2111 <ganeshlakade2111@gmail.com>
2024-10-26 11:43:44 +0530

    test case pass for empty string

---
+++ StringCalculator/src/main/java/com/example/StringCalculator/Stringcal.java
@@ -0,0 +1,14 @@
+package com.example.StringCalculator;
+
+public class Stringcal {
+
+    public static int add(String num)
+    {
+        if(num.isEmpty())
+        {
+            return 0;
+        }
+
+        return 1;
+    }
+}
